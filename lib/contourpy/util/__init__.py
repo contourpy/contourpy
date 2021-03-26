8569624d1fd3a54cf8d8b0547123975e6a719ae2
Ian Thomas <ianthomas23@gmail.com>
2021-03-26 11:48:05 +0000

    Wrap bokeh import in try..except

--- lib/contourpy/util/__init__.py
+++ lib/contourpy/util/__init__.py
@@ -1,3 +1,8 @@
 from .data import random_uniform
-from .bokeh_renderer import BokehRenderer
 from .mpl_renderer import MplRenderer, MplTestRenderer, MplDebugRenderer
+
+try:
+    # Bokeh may not be available.  Fail silently.
+    from .bokeh_renderer import BokehRenderer
+except:
+    pass
