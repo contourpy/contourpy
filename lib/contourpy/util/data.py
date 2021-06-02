8bbb7468af5ca9ddd196707cc295b29016aa6cd5
Ian Thomas <ianthomas23@gmail.com>
2021-06-02 13:54:16 +0100

    Use numpy.random.default_rng (#32)

--- lib/contourpy/util/data.py
+++ lib/contourpy/util/data.py
@@ -7,12 +7,12 @@ def random_uniform(shape, seed=2187, mask_fraction=0.0):
     y = np.arange(ny, dtype=np.float64)
     x, y = np.meshgrid(x, y)
 
-    np.random.seed(seed)
-    z = np.random.uniform(size=shape)
+    rng = np.random.default_rng(seed)
+    z = rng.uniform(size=shape)
 
     if mask_fraction > 0.0:
         mask_fraction = min(mask_fraction, 0.99)
-        mask = np.random.uniform(size=shape) < mask_fraction
+        mask = rng.uniform(size=shape) < mask_fraction
         z = np.ma.array(z, mask=mask)
 
     return x, y, z
