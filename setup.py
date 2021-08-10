f0ca28a688f3a21f5d74bbe1fe935da6040e8c5d
Ian Thomas <ianthomas23@gmail.com>
2021-08-10 16:04:56 +0100

    Move static setup data to setup.cfg

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import numpy as np  # Needed to compile mpl2005.c which doesn't use pybind11.
 import os
 import re
-from setuptools import Extension, find_packages, setup
+from setuptools import Extension, setup
 from pybind11.setup_helpers import build_ext, naive_recompile, ParallelCompile, Pybind11Extension
 
 
@@ -89,19 +89,8 @@ def read_requirements(filename):
 
 
 setup(
-    name="contourpy",
     version=__version__,
-    description="2D contouring",
-    long_description="Calculating contours of 2D quadrilateral grids",
-    author="Ian Thomas",
-    author_email="ianthomas23@gmail.com",
-    url="https://github.com/contourpy/contourpy",
-    license="BSD",
-    package_dir={"": "lib"},
-    packages=find_packages("lib"),
     ext_modules=ext_modules,
     cmdclass=cmdclass,
-    zip_safe=False,
-    python_requires=">=3.7",
     install_requires=read_requirements("requirements/install.txt"),
 )
