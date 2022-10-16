35a3123727bcc8cff7b478e87caa9939e7c90b61
Ian Thomas <ianthomas23@gmail.com>
2022-10-16 18:09:22 +0100

    Add more constructor tests

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-import contourpy
+from contourpy import ContourGenerator, FillType, LineType, ZInterp, contour_generator, max_threads
 
 from . import util_test
 
@@ -34,14 +34,14 @@ def xyz_7x5_as_arrays():  # shape (7, 5)
 def test_ndim_z(xyz_3x3_as_lists, name, z):
     x, y, _ = xyz_3x3_as_lists
     with pytest.raises(TypeError):
-        contourpy.contour_generator(x, y, z, name=name)
+        contour_generator(x, y, z, name=name)
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
 @pytest.mark.parametrize("all_xyz", ([[1]], [[1], [2]], [[1, 2]]))
 def test_z_shape_too_small(all_xyz, name):
     with pytest.raises(TypeError):
-        contourpy.contour_generator(all_xyz, all_xyz, all_xyz, name=name)
+        contour_generator(all_xyz, all_xyz, all_xyz, name=name)
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
@@ -49,35 +49,43 @@ def test_z_shape_too_small(all_xyz, name):
 def test_diff_ndim_xy(xyz_3x3_as_lists, name, wrong_ndim):
     x, y, z = xyz_3x3_as_lists
     with pytest.raises(TypeError):
-        contourpy.contour_generator(wrong_ndim, y, z, name=name)
+        contour_generator(wrong_ndim, y, z, name=name)
     with pytest.raises(TypeError):
-        contourpy.contour_generator(x, wrong_ndim, z, name=name)
+        contour_generator(x, wrong_ndim, z, name=name)
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
 def test_xy_None(xyz_3x3_as_lists, name):
     _, _, z = xyz_3x3_as_lists
-    contourpy.contour_generator(None, None, z, name=name)
+    contour_generator(None, None, z, name=name)
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
 def test_xy_not_specified(xyz_3x3_as_lists, name):
     _, _, z = xyz_3x3_as_lists
-    contourpy.contour_generator(z=z, name=name)
+    contour_generator(z=z, name=name)
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
 def test_xy_1d(name):
     z = [[0, 1, 2], [3, 4, 5]]
-    contourpy.contour_generator([0, 1, 2], [0, 1], z, name=name)
+    contour_generator([0, 1, 2], [0, 1], z, name=name)
     with pytest.raises(TypeError):
-        contourpy.contour_generator([0, 1], [0, 1], z, name=name)
+        contour_generator([0, 1], [0, 1], z, name=name)
     with pytest.raises(TypeError):
-        contourpy.contour_generator([0, 1, 2, 3], [0, 1], z, name=name)
+        contour_generator([0, 1, 2, 3], [0, 1], z, name=name)
     with pytest.raises(TypeError):
-        contourpy.contour_generator([0, 1, 2], [0], z, name=name)
+        contour_generator([0, 1, 2], [0], z, name=name)
     with pytest.raises(TypeError):
-        contourpy.contour_generator([0, 1, 2], [0, 1, 2], z, name=name)
+        contour_generator([0, 1, 2], [0, 1, 2], z, name=name)
+
+
+@pytest.mark.parametrize("name", util_test.all_names())
+def test_xy_ndim_more_than_2(name):
+    z = [[0, 1, 2], [3, 4, 5]]
+    msg = "Inputs x and y must be None, 1D or 2D, not 3D"
+    with pytest.raises(TypeError, match=msg):
+        contour_generator([[[1]]], [[[2]]], z, name=name)
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
@@ -85,19 +93,27 @@ def test_xy_1d(name):
 def test_xyz_diff_shapes(xyz_3x3_as_lists, name, diff_shape):
     x, y, z = xyz_3x3_as_lists
     with pytest.raises(TypeError):
-        contourpy.contour_generator(diff_shape, y, z, name=name)
+        contour_generator(diff_shape, y, z, name=name)
     with pytest.raises(TypeError):
-        contourpy.contour_generator(x, diff_shape, z, name=name)
+        contour_generator(x, diff_shape, z, name=name)
     with pytest.raises(TypeError):
-        contourpy.contour_generator(x, y, diff_shape, name=name)
+        contour_generator(x, y, diff_shape, name=name)
 
 
 @pytest.mark.parametrize("name", util_test.corner_mask_names())
 def test_corner_mask(xyz_3x3_as_lists, name):
     x, y, z = xyz_3x3_as_lists
     for corner_mask in [False, True]:
-        cont_gen = contourpy.contour_generator(x, y, z, name=name, corner_mask=corner_mask)
+        cont_gen = contour_generator(x, y, z, name=name, corner_mask=corner_mask)
         assert cont_gen.corner_mask == corner_mask
+
+
+def test_corner_mask_not_supported(xyz_3x3_as_lists):
+    name = "mpl2005"
+    x, y, z = xyz_3x3_as_lists
+    msg = f"{name} contour generator does not support corner_mask=True"
+    with pytest.raises(ValueError, match=msg):
+        contour_generator(x, y, z, name=name, corner_mask=True)
 
 
 @pytest.mark.parametrize('name', util_test.all_names())
@@ -105,11 +121,11 @@ def test_chunk_size_negative(xyz_3x3_as_lists, name):
     x, y, z = xyz_3x3_as_lists
     msg = "chunk_size cannot be negative"
     with pytest.raises(ValueError, match=msg):
-        contourpy.contour_generator(x, y, z, name=name, chunk_size=-1)
+        contour_generator(x, y, z, name=name, chunk_size=-1)
     with pytest.raises(ValueError, match=msg):
-        contourpy.contour_generator(x, y, z, name=name, chunk_size=(-1, 0))
+        contour_generator(x, y, z, name=name, chunk_size=(-1, 0))
     with pytest.raises(ValueError, match=msg):
-        contourpy.contour_generator(x, y, z, name=name, chunk_size=(0, -1))
+        contour_generator(x, y, z, name=name, chunk_size=(0, -1))
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
@@ -117,7 +133,7 @@ def test_chunk_size_negative(xyz_3x3_as_lists, name):
 def test_chunk_size_1d(xyz_7x5_as_arrays, name, chunk_size):
     x, y, z = xyz_7x5_as_arrays
     ny, nx = z.shape
-    cont_gen = contourpy.contour_generator(x, y, z, name=name, chunk_size=chunk_size)
+    cont_gen = contour_generator(x, y, z, name=name, chunk_size=chunk_size)
     ret_y_chunk_size, ret_x_chunk_size = cont_gen.chunk_size
     ret_y_chunk_count, ret_x_chunk_count = cont_gen.chunk_count
     if chunk_size == 0:
@@ -138,8 +154,7 @@ def test_chunk_size_1d(xyz_7x5_as_arrays, name, chunk_size):
 def test_chunk_size_2d(xyz_7x5_as_arrays, name, x_chunk_size, y_chunk_size):
     x, y, z = xyz_7x5_as_arrays
     ny, nx = z.shape
-    cont_gen = contourpy.contour_generator(
-        x, y, z, name=name, chunk_size=(y_chunk_size, x_chunk_size))
+    cont_gen = contour_generator(x, y, z, name=name, chunk_size=(y_chunk_size, x_chunk_size))
     ret_y_chunk_size, ret_x_chunk_size = cont_gen.chunk_size
     ret_y_chunk_count, ret_x_chunk_count = cont_gen.chunk_count
     if x_chunk_size == 0:
@@ -161,11 +176,11 @@ def test_chunk_size_and_count(xyz_7x5_as_arrays):
     name = "serial"
     msg = "Only one of chunk_size, chunk_count and total_chunk_count should be set"
     with pytest.raises(ValueError, match=msg):
-        contourpy.contour_generator(x, y, z, name=name, chunk_size=1, chunk_count=1)
+        contour_generator(x, y, z, name=name, chunk_size=1, chunk_count=1)
     with pytest.raises(ValueError, match=msg):
-        contourpy.contour_generator(x, y, z, name=name, chunk_size=1, total_chunk_count=1)
+        contour_generator(x, y, z, name=name, chunk_size=1, total_chunk_count=1)
     with pytest.raises(ValueError, match=msg):
-        contourpy.contour_generator(x, y, z, name=name, chunk_count=1, total_chunk_count=1)
+        contour_generator(x, y, z, name=name, chunk_count=1, total_chunk_count=1)
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
@@ -176,7 +191,7 @@ def test_chunk_size_and_count(xyz_7x5_as_arrays):
 def test_chunk_count_1d(xyz_7x5_as_arrays, name, chunk_count, ret_chunk_count):
     x, y, z = xyz_7x5_as_arrays
     ny, nx = z.shape
-    cont_gen = contourpy.contour_generator(x, y, z, name=name, chunk_count=chunk_count)
+    cont_gen = contour_generator(x, y, z, name=name, chunk_count=chunk_count)
     ret_y_chunk_size, ret_x_chunk_size = cont_gen.chunk_size
     assert cont_gen.chunk_count == ret_chunk_count
     ret_y_chunk_count, ret_x_chunk_count = ret_chunk_count
@@ -195,7 +210,7 @@ def test_chunk_count_1d(xyz_7x5_as_arrays, name, chunk_count, ret_chunk_count):
 def test_chunk_count_2d(xyz_7x5_as_arrays, name, chunk_count, ret_chunk_count):
     x, y, z = xyz_7x5_as_arrays
     ny, nx = z.shape
-    cont_gen = contourpy.contour_generator(x, y, z, name=name, chunk_count=chunk_count)
+    cont_gen = contour_generator(x, y, z, name=name, chunk_count=chunk_count)
     ret_y_chunk_size, ret_x_chunk_size = cont_gen.chunk_size
     assert cont_gen.chunk_count == ret_chunk_count
     ret_y_chunk_count, ret_x_chunk_count = ret_chunk_count
@@ -214,7 +229,7 @@ def test_chunk_count_2d(xyz_7x5_as_arrays, name, chunk_count, ret_chunk_count):
 def test_total_chunk_count(xyz_7x5_as_arrays, name, total_chunk_count, ret_chunk_count):
     x, y, z = xyz_7x5_as_arrays
     ny, nx = z.shape
-    cont_gen = contourpy.contour_generator(x, y, z, name=name, total_chunk_count=total_chunk_count)
+    cont_gen = contour_generator(x, y, z, name=name, total_chunk_count=total_chunk_count)
     ret_y_chunk_size, ret_x_chunk_size = cont_gen.chunk_size
     assert cont_gen.chunk_count == ret_chunk_count
     ret_y_chunk_count, ret_x_chunk_count = ret_chunk_count
@@ -229,14 +244,35 @@ def test_name_invalid(xyz_3x3_as_lists):
     name = "some invalid name"
     msg = f"Unrecognised contour generator name: {name}"
     with pytest.raises(ValueError, match=msg):
-        _ = contourpy.contour_generator(x, y, z, name=name)
+        _ = contour_generator(x, y, z, name=name)
+
+
+@pytest.mark.parametrize("name", ["mpl2005", "mpl2014"])
+@pytest.mark.parametrize(
+    "line_type", [LineType.Separate, LineType.ChunkCombinedCode, LineType.ChunkCombinedOffset])
+def test_line_type_not_supported(xyz_3x3_as_lists, name, line_type):
+    x, y, z = xyz_3x3_as_lists
+    msg = f"{name} contour generator does not support line_type {line_type}"
+    with pytest.raises(ValueError, match=msg):
+        contour_generator(x, y, z, name=name, line_type=line_type)
+
+
+@pytest.mark.parametrize("name", ["mpl2005", "mpl2014"])
+@pytest.mark.parametrize("fill_type", [
+    FillType.OuterOffset, FillType.ChunkCombinedCode, FillType.ChunkCombinedOffset,
+    FillType.ChunkCombinedCodeOffset, FillType.ChunkCombinedOffsetOffset])
+def test_fill_type_not_supported(xyz_3x3_as_lists, name, fill_type):
+    x, y, z = xyz_3x3_as_lists
+    msg = f"{name} contour generator does not support fill_type {fill_type}"
+    with pytest.raises(ValueError, match=msg):
+        contour_generator(x, y, z, name=name, fill_type=fill_type)
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
 def test_properties(xyz_3x3_as_lists, name):
     # Test that each ContourGenerator class implements all the required properties.
     x, y, z = xyz_3x3_as_lists
-    cont_gen = contourpy.contour_generator(x, y, z, name=name)
+    cont_gen = contour_generator(x, y, z, name=name)
     _, _ = cont_gen.chunk_count
     _, _ = cont_gen.chunk_size
     _ = cont_gen.corner_mask
@@ -251,8 +287,16 @@ def test_properties(xyz_3x3_as_lists, name):
 def test_quad_as_tri(xyz_3x3_as_lists, name):
     x, y, z = xyz_3x3_as_lists
     for quad_as_tri in [False, True]:
-        cont_gen = contourpy.contour_generator(x, y, z, name=name, quad_as_tri=quad_as_tri)
+        cont_gen = contour_generator(x, y, z, name=name, quad_as_tri=quad_as_tri)
         assert cont_gen.quad_as_tri == quad_as_tri
+
+
+def test_quad_as_tri_not_supported(xyz_3x3_as_lists):
+    x, y, z = xyz_3x3_as_lists
+    name = "mpl2005"
+    msg = f"{name} contour generator does not support quad_as_tri=True"
+    with pytest.raises(ValueError, match=msg):
+        contour_generator(x, y, z, name=name, quad_as_tri=True)
 
 
 @pytest.mark.parametrize("chunk_size", [0, 1, 2])
@@ -260,31 +304,55 @@ def test_quad_as_tri(xyz_3x3_as_lists, name):
 def test_thread_count(xyz_7x5_as_arrays, chunk_size, thread_count):
     name = "threaded"
     x, y, z = xyz_7x5_as_arrays
-    cont_gen = contourpy.contour_generator(
+    cont_gen = contour_generator(
         x, y, z, name=name, chunk_size=chunk_size, thread_count=thread_count)
     ret_thread_count = cont_gen.thread_count
     ret_chunk_count = np.prod(cont_gen.chunk_count)
-    max_threads = contourpy.max_threads()
+    max_thread_count = max_threads()
     if chunk_size == 0:
         assert ret_chunk_count == 1
         assert ret_thread_count == 1
     elif thread_count == 0:
-        assert ret_thread_count == min(max_threads, ret_chunk_count)
+        assert ret_thread_count == min(max_thread_count, ret_chunk_count)
     else:
-        assert ret_thread_count == min(max_threads, ret_chunk_count, ret_thread_count)
+        assert ret_thread_count == min(max_thread_count, ret_chunk_count, ret_thread_count)
+
+
+@pytest.mark.parametrize("name", util_test.all_names(exclude="threaded"))
+def test_thread_count_not_supported(xyz_3x3_as_lists, name):
+    x, y, z = xyz_3x3_as_lists
+    thread_count = 2
+    msg = f"{name} contour generator does not support thread_count {thread_count}"
+    with pytest.raises(ValueError, match=msg):
+        contour_generator(x, y, z, name=name, thread_count=thread_count)
 
 
 def test_enums_as_strings(xyz_3x3_as_lists):
     x, y, z = xyz_3x3_as_lists
-    cg = contourpy.contour_generator(
+    cg = contour_generator(
         x, y, z, line_type="SeparateCode", fill_type="ChunkCombinedCodeOffset", z_interp="Log")
-    assert cg.line_type == contourpy.LineType.SeparateCode
-    assert cg.fill_type == contourpy.FillType.ChunkCombinedCodeOffset
-    assert cg.z_interp == contourpy.ZInterp.Log
+    assert cg.line_type == LineType.SeparateCode
+    assert cg.fill_type == FillType.ChunkCombinedCodeOffset
+    assert cg.z_interp == ZInterp.Log
 
 
 @pytest.mark.parametrize("name", util_test.all_names())
 def test_is_contour_generator(name, xyz_3x3_as_lists):
     x, y, z = xyz_3x3_as_lists
-    cg = contourpy.contour_generator(x, y, z, name=name)
-    assert isinstance(cg, contourpy.ContourGenerator)
+    cg = contour_generator(x, y, z, name=name)
+    assert isinstance(cg, ContourGenerator)
+
+
+@pytest.mark.parametrize("name", util_test.all_names())
+def test_z_interp_none_to_linear(name, xyz_3x3_as_lists):
+    x, y, z = xyz_3x3_as_lists
+    cg = contour_generator(x, y, z, name=name, z_interp=None)
+    assert cg.z_interp == ZInterp.Linear
+
+
+@pytest.mark.parametrize("name", ["mpl2005", "mpl2014"])
+def test_z_interp_not_supported(name, xyz_3x3_as_lists):
+    x, y, z = xyz_3x3_as_lists
+    msg = f"{name} contour generator does not support z_interp ZInterp.Log"
+    with pytest.raises(ValueError, match=msg):
+        contour_generator(x, y, z, name=name, z_interp=ZInterp.Log)
