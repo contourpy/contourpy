2880c5d28a924f88506acfe2cc3017e86a300fb0
Ian Thomas <ianthomas23@gmail.com>
2023-04-02 19:06:35 +0100

    Remove typehints from API docs

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,8 @@ extensions = [
     "plot_directive",
 ]
 
+autodoc_typehints = "none"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
