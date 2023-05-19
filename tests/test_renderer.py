67a92553cac20767ffd3596f0ead114eaea99cad
Ian Thomas <ianthomas23@gmail.com>
2023-05-19 09:39:44 +0100

    Debug renderer tests (#244)
    * Add MplDebugRenderer tests
    
    * Add mask to bokeh/mpl renderer tests
    
    * Separate text and no_text baseline images
    
    * Type hints for figsize of Renderer classes

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 from contourpy import FillType, LineType, contour_generator
-from contourpy.util.data import random
+from contourpy.util.data import random, simple
 
 
 def test_chrome_version() -> None:
@@ -27,6 +27,61 @@ def test_chrome_version() -> None:
 @pytest.mark.text
 @pytest.mark.parametrize("show_text", [False, True])
 @pytest.mark.parametrize("fill_type", FillType.__members__.values())
+def test_debug_renderer_filled(show_text: bool, fill_type: FillType) -> None:
+    from contourpy.util.mpl_renderer import MplDebugRenderer
+
+    from .image_comparison import compare_images
+
+    renderer = MplDebugRenderer(figsize=(4.5, 3), show_frame=show_text)
+    x, y, z = simple((3, 4))
+    cont_gen = contour_generator(x, y, z, fill_type=fill_type)
+    levels = [0.25, 0.6]
+
+    filled = cont_gen.filled(levels[0], levels[1])
+    renderer.filled(filled, fill_type, color="C1")
+
+    renderer.grid(x, y)
+    if show_text:
+        renderer.quad_numbers(x, y, z)
+        renderer.z_levels(x, y, z, lower_level=levels[0], upper_level=levels[1])
+
+    image_buffer = renderer.save_to_buffer()
+    suffix = "" if show_text else "_no_text"
+    compare_images(image_buffer, f"debug_renderer_filled{suffix}.png", f"{fill_type}")
+
+
+@pytest.mark.image
+@pytest.mark.text
+@pytest.mark.parametrize("show_text", [False, True])
+@pytest.mark.parametrize("line_type", LineType.__members__.values())
+def test_debug_renderer_lines(show_text: bool, line_type: LineType) -> None:
+    from contourpy.util.mpl_renderer import MplDebugRenderer
+
+    from .image_comparison import compare_images
+
+    renderer = MplDebugRenderer(figsize=(4.5, 3), show_frame=show_text)
+    x, y, z = simple((3, 4))
+    cont_gen = contour_generator(x, y, z, line_type=line_type)
+    levels = [0.25, 0.6]
+
+    for i, level in enumerate(levels):
+        lines = cont_gen.lines(level)
+        renderer.lines(lines, line_type, color=f"C{i}", linewidth=2)
+
+    renderer.grid(x, y)
+    if show_text:
+        renderer.quad_numbers(x, y, z)
+        renderer.point_numbers(x, y, z)
+
+    image_buffer = renderer.save_to_buffer()
+    suffix = "" if show_text else "_no_text"
+    compare_images(image_buffer, f"debug_renderer_lines{suffix}.png", f"{line_type}")
+
+
+@pytest.mark.image
+@pytest.mark.text
+@pytest.mark.parametrize("show_text", [False, True])
+@pytest.mark.parametrize("fill_type", FillType.__members__.values())
 @pytest.mark.parametrize("renderer_type", ["mpl", "bokeh"])
 def test_renderer_filled(show_text: bool, fill_type: FillType, renderer_type: str) -> None:
     if renderer_type == "bokeh":
@@ -41,7 +96,7 @@ def test_renderer_filled(show_text: bool, fill_type: FillType, renderer_type: st
 
     from .image_comparison import compare_images
 
-    x, y, z = random((3, 4))
+    x, y, z = random((3, 4), mask_fraction=0.35)
     renderer = Renderer(ncols=2, figsize=(8, 3), show_frame=False)
     for ax, quad_as_tri in enumerate((False, True)):
         cont_gen = contour_generator(x, y, z, fill_type=fill_type)
@@ -56,6 +111,7 @@ def test_renderer_filled(show_text: bool, fill_type: FillType, renderer_type: st
 
         if quad_as_tri:
             renderer.grid(x, y, ax=ax, alpha=0.5, quad_as_tri_alpha=0.5)
+            renderer.mask(x, y, z, ax=ax, color="red")
             if show_text:
                 renderer.title("Title", ax=ax)
         else:
@@ -89,7 +145,7 @@ def test_renderer_lines(show_text: bool, line_type: LineType, renderer_type: str
 
     from .image_comparison import compare_images
 
-    x, y, z = random((3, 4))
+    x, y, z = random((3, 4), mask_fraction=0.35)
     renderer = Renderer(ncols=2, figsize=(8, 3), show_frame=show_text)
     for ax, quad_as_tri in enumerate((False, True)):
         cont_gen = contour_generator(x, y, z, line_type=line_type)
@@ -104,6 +160,7 @@ def test_renderer_lines(show_text: bool, line_type: LineType, renderer_type: str
 
         if quad_as_tri:
             renderer.grid(x, y, ax=ax, alpha=0.2, quad_as_tri_alpha=0.2)
+            renderer.mask(x, y, z, ax=ax, color="red")
             if show_text:
                 renderer.z_values(x, y, z, ax=ax, fmt=".2f", quad_as_tri=True)
                 renderer.title("Title", ax=ax)
