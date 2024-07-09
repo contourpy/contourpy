e92a40a643435e172cc62a6690fbfe7d73ba1836
Ian Thomas <ianthomas23@gmail.com>
2024-07-09 13:14:10 +0100

    non-np nan

--- src/util.cpp
+++ src/util.cpp
@@ -2,19 +2,24 @@
 #include <cmath>
 #include <thread>
 
+
+#include <limits>
+
+
 namespace contourpy {
 
 bool Util::_nan_loaded = false;
 
-double Util::nan = 0.0;
+//double Util::nan = 0.0;
+double Util::nan = std::numeric_limits<double>::quiet_NaN();
 
 void Util::ensure_nan_loaded()
 {
-    if (!_nan_loaded) {
+    /*if (!_nan_loaded) {
         auto numpy = py::module_::import("numpy");
         nan = numpy.attr("nan").cast<double>();
         _nan_loaded = true;
-    }
+    }*/
 }
 
 index_t Util::get_max_threads()
